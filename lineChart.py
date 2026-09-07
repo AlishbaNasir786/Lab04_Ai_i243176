c66ac4f324b47d6f1e6837b0e9cb3d40ac5102d2
AlishbaNasir786 <alishbanasir11102004@gmail.com>
2026-09-07 13:24:00 +0500

    Add matplotlib dependency

--- lineChart.py
+++ lineChart.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-
-
 # ---------------------------------------------------
 # PAGE CONFIGURATION
 # ---------------------------------------------------
